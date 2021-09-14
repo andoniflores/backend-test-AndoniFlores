8b05943f5b41d448a4bdf87ebab4fa4c9ef08aa8
andoniflores <andonixd@gmail.com>
2021-09-14 00:55:14 -0300

    Added webhook url in keys.py file and added to gitignore

--- cornershop-backend-test/menus/tasks.py
+++ cornershop-backend-test/menus/tasks.py
@@ -2,10 +2,11 @@ import time
 
 from celery import shared_task
 import requests
+from keys import WEBHOOK_URL
 
 # Send slack message to slack cornershop channel
 @shared_task
 def send_reminder_task(payload):
-    response = requests.post('https://hooks.slack.com/services/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',
+    response = requests.post(WEBHOOK_URL,
                     data = payload)
     return response

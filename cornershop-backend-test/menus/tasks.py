9593ef6237e58651b4264a424ac65162819ec22c
andoniflores <andonixd@gmail.com>
2021-09-14 00:32:37 -0300

    fixed webhook with proper github installation

--- cornershop-backend-test/menus/tasks.py
+++ cornershop-backend-test/menus/tasks.py
@@ -6,6 +6,6 @@ import requests
 # Send slack message to slack cornershop channel
 @shared_task
 def send_reminder_task(payload):
-    response = requests.post('https://hooks.slack.com/services/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',
+    response = requests.post('https://hooks.slack.com/services/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',
                     data = payload)
     return response
